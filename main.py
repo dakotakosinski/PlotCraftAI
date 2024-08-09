fe6a506089201e056e08a6ba40ade4d814d8db20
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-08-09 09:55:16 -0700

    Madlibs Updated

--- main.py
+++ main.py
@@ -16,17 +16,23 @@ def home():
 def story_form():
     return render_template('story_form.html')
 
+@app.route('/madlibs_form')
+def madlibs_form():
+    return render_template('madlibs_form.html')
+
 @app.route("/generate_story", methods=["POST"])
 def generate_story():
     prompt = request.form.get('prompt')
     word_cap = request.form.get('word_cap')
     genre = request.form.get('genre')
+    protagonist = request.form.get('protagonist')
+    brainrot = request.form.get('brainrot')
 
     my_thread = client.beta.threads.create()
     my_message = client.beta.threads.messages.create(
         thread_id=my_thread.id,
         role='user',
-        content=f"{prompt} {genre} {word_cap}"
+        content=f"the prompt: {prompt} the protagonist: {protagonist}, the: {genre} the word cap: {word_cap}, Brainrot Mode: {brainrot}"
     )
     my_run = client.beta.threads.runs.create(
         thread_id=my_thread.id,
@@ -50,6 +56,39 @@ def generate_story():
         response = all_messages.data[0].content[0].text.value
         return render_template('story_result.html', story=response.strip())
     return "Story generation failed."
+
+@app.route('/generate_madlibs', methods=['POST'])
+def generate_madlibs():
+    genre = request.form.get('genre')
+    my_thread = client.beta.threads.create()
+    my_message = client.beta.threads.messages.create(
+        thread_id=my_thread.id,
+        role='user',
+        content=f"the Genere for the Madlib is {genre}"
+    )
+    my_run = client.beta.threads.runs.create(
+        thread_id=my_thread.id,
+        assistant_id=plotCraftAI.id
+    )
+    while my_run.status in ["queued", "in_progress"]:
+        keep_retrieving_run = client.beta.threads.runs.retrieve(
+            thread_id=my_thread.id,
+            run_id=my_run.id
+        )
+        if keep_retrieving_run.status in ['queued', 'in_progress']:
+            continue
+        else:
+            print(f"Run Status: {keep_retrieving_run.status}")
+            break
+
+    if keep_retrieving_run.status == "completed":
+        all_messages = client.beta.threads.messages.list(
+            thread_id=my_thread.id
+        )
+        response = all_messages.data[0].content[0].text.value
+        return render_template('madlibs_story.html', story=response.strip())
+    return "Story generation failed."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
